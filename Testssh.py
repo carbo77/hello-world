c62f4cc00427740ad20dd1f04ab19da61129d360
Wahib <carbo77@hotmail.com>
2020-02-21 11:19:42 -0700

    BHW Backup config working - Multiple Devices on List

--- Testssh.py
+++ Testssh.py
@@ -1,22 +1,31 @@
 from netmiko import ConnectHandler
 
-MyDevices = {
+SW1 = {
     'device_type': 'cisco_ios',
     'ip': '10.3.254.22',
     'username': 'wbohsainadm',
     'password': 'Maybe20?'
 }
 
-connected = ConnectHandler(**MyDevices)
-output = connected.send_command('terminal lenght 0')
-output = connected.send_command('show run')
-ConfigBckp = open(MyDevices['ip']+".txt","w")
+SW2 = {
+    'device_type': 'cisco_ios',
+    'ip': '10.3.254.23',
+    'username': 'wbohsainadm',
+    'password': 'Maybe20?'
+}
 
-ConfigBckp.write(output)
-ConfigBckp.write("\n")
-ConfigBckp.write("by WB")
-ConfigBckp.close()
-print (output)
+Switches = [SW1,SW2]
+
+for Devices in Switches:
+    connected = ConnectHandler(**Devices)
+    output = connected.send_command('terminal lenght 0')
+    output = connected.send_command('show run')
+    ConfigBckp = open(Devices['ip']+".txt","w")
+    ConfigBckp.write(output)
+    ConfigBckp.write("\n")
+    ConfigBckp.write("by WB")
+    ConfigBckp.close()
+    print (output)
 
 
 
